0754b5d6cbb2023db06e040a4e04b83c938593c1
Sapir Ohava <sapir@apptimus.media>
2022-01-25 12:30:32 +0200

    finish related articles

--- app.js
+++ app.js
@@ -1,46 +1,57 @@
-const relatedArticles = [
+var relatedArticles = [
   {
-    title: "best travel places for fall",
-    img: "./photos/autumn.jpeg",
+    title: "These Destinations Don't Quite Match Peoples Expectations",
+    img: "http://i.imgur.com/Yl5JfFv.jpg",
     url: "https://www.google.com/",
   },
   {
-    title: "things you didnt noticed happend on avengers",
-    img: "./photos/avengers.jpg",
+    title: "These Destinations Don't Quite Match Peoples Expectations",
+    img: "http://i.imgur.com/Yl5JfFv.jpg",
     url: "https://www.google.com/",
   },
   {
-    title: "the most cutest dogs you have ever seen!",
-    img: "./photos/BeFunky.jpg",
+    title: "These Destinations Don't Quite Match Peoples Expectations",
+    img: "http://i.imgur.com/Yl5JfFv.jpg",
     url: "https://www.google.com/",
   },
   {
-    title: "most dangerous places to travel",
-    img: "./photos/a1.jpg",
+    title: "These Destinations Don't Quite Match Peoples Expectations",
+    img: "http://i.imgur.com/Yl5JfFv.jpg",
     url: "https://www.google.com/",
   },
   {
-    title: "shopping secrets",
-    img: "./photos/shopping.jpg",
+    title: "These Destinations Don't Quite Match Peoples Expectations",
+    img: "http://i.imgur.com/Yl5JfFv.jpg",
+    url: "https://www.google.com/",
+  },
+  {
+    title: "These Destinations Don't Quite Match Peoples Expectations",
+    img: "http://i.imgur.com/Yl5JfFv.jpg",
     url: "https://www.google.com/",
   },
 ];
 
-let element = document.getElementById("relatedDiv");
-relatedArticles.forEach((item) => {
-  let div = document.createElement("div");
-  div.className = "related-article-img-div";
-  let image = document.createElement("img");
-  let anchor = document.createElement("a");
-  anchor.setAttribute("href", item.url);
-  image.setAttribute("src", item.img);
-  image.className = "related-article-image";
-  let title = document.createElement("label");
-  title.textContent = item.title;
-  //   title.className = "related-article-title";
-  anchor.className = "related-article-anchor";
-  div.appendChild(image);
-  div.appendChild(title);
-  anchor.appendChild(div);
-  element.appendChild(anchor);
-});
+function createArticleRecommendations(articlesList, containerDivID) {
+  function getArticleDiv(articleObject) {
+    var div = document.createElement("div");
+    div.className = "related-article-img-div";
+    var image = document.createElement("img");
+    var anchor = document.createElement("a");
+    anchor.setAttribute("href", articleObject.url);
+    image.setAttribute("src", articleObject.img);
+    image.className = "related-article-image";
+    var title = document.createElement("label");
+    title.textContent = articleObject.title;
+    title.className = "related-article-title";
+    anchor.className = "related-article-anchor";
+    anchor.appendChild(image);
+    anchor.appendChild(title);
+    div.appendChild(anchor);
+    containerDivID.appendChild(div);
+  }
+  for (var i = 0; i < articlesList.length; i++) {
+    getArticleDiv(articlesList[i]);
+  }
+}
+var element = document.getElementById("related-div");
+createArticleRecommendations(relatedArticles, element);
