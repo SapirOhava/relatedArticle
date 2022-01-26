a87f9e585ec62cd3879548e33c6ffc026a0b9caa
Sapir Ohava <sapir@apptimus.media>
2022-01-26 17:08:58 +0200

    more fixes

--- app.js
+++ app.js
@@ -1,9 +1,55 @@
-// function displayAdblockerAlert(containerID, websiteLogoURL) {
-//   var container = document.getElementById(containerID);
-//   var modalDiv = document.createElement("div");
-//   modalDiv.className = "adb-modal-div";
+function displayAdblockerAlert(containerID, websiteLogoURL, siteName) {
+  var container = document.getElementById(containerID);
+  var modalDiv = document.createElement("div");
+  modalDiv.className = "adb-modal-div";
+  modalDiv.innerHTML =
+    `  <div class="adb-div">
+        <div class="adb-logo-div">
+          <div class="adb-logo-img-div">
+            <img class="adb-logo-img" src="` +
+    websiteLogoURL +
+    `"/>
+          </div>
+          <span class="adb-title-adbolck-span"
+            >It looks like your ad blocker is enabled.</span
+          >
+          <span class="adb-sub-title-adbolck-span">
+            In order to continue to enjoy the contents of the site, please
+            whitelist our site.
+          </span>
+        </div>
+        <div class="adb-disable-adblock-div">
+          <div>
+            <span class="adb-turn-off-adblocker-span"
+              >Disable Adblock:</span
+            >
+            <ul class="adb-turn-off-adblocker-ul">
+              <li>Click the ad blocker at the top of the browser</li>
+              <li>Unblock ` +
+    siteName +
+    `</li>
+              <li>Reload your browser or click the button below to continue</li>
+            </ul>
+          </div>
 
-//   container.appendChild(modalDiv);
-// }
-// var btn = document.getElementById("open-modal");
-// btn.addEventListener("click", displayAdblockerAlert);
+          <img class="adb-img-adblock" src="./photos/adblock.png" />
+        </div>
+        <div class="adb-btn-div">
+          <button id="adb-reload-btn" class="adb-btn-adblock">Reload Website</button>
+        </div>
+      </div>`;
+
+  container.appendChild(modalDiv);
+}
+var openModalBtn = document.getElementById("adb-open-modal");
+openModalBtn.addEventListener("click", () => {
+  displayAdblockerAlert(
+    "container",
+    "./photos/logo_v2.jpg",
+    "WordTraveling.com"
+  );
+});
+var reloadBtn = document.getElementById("adb-reload-btn");
+reloadBtn.addEventListener("click", () => {
+  window.location.reload();
+});
