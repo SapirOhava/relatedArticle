39ac196eed65045351b53dd0142444a054af28fa
Sapir Ohava <sapir@apptimus.media>
2022-01-26 15:44:08 +0200

    adblock fixes

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-function displayAdblockerAlert(containerID, websiteLogoURL) {
-  var container = document.getElementById(containerID);
-  var modalDiv = document.createElement("div");
-  modalDiv.className = "adb-modal-div";
-  
-  container.appendChild(modalDiv);
-}
-var btn = document.getElementById("open-modal");
-btn.addEventListener("click", displayAdblockerAlert);
+// function displayAdblockerAlert(containerID, websiteLogoURL) {
+//   var container = document.getElementById(containerID);
+//   var modalDiv = document.createElement("div");
+//   modalDiv.className = "adb-modal-div";
+
+//   container.appendChild(modalDiv);
+// }
+// var btn = document.getElementById("open-modal");
+// btn.addEventListener("click", displayAdblockerAlert);
